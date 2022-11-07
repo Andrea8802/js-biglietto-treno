8a8c29c9abdd6a8c0db3aed4c824b155f534bf81
Andrea8802 <andreabonaventura802@gmail.com>
2022-11-07 15:49:16 +0100

    Variables Done

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// Calcolo totale prezzo del viaggio
+
+// Dichiarazioni variabili
+let kmDaPercorrere
+let etaPasseggero
+const prezzoBiglietto = 0.21
+let prezzoViaggio
+let prezzoScontato
+let prezzoFinale
+
+// Chiedere quanti km vuole percorrere
+
+
+// Chiedere età passeggero
+
+// Prezzo 0.21€ 
+
+// Moltiplicare km X 0.21€
+
+// Se è minorenne si applicherà uno sconto del 20%
+
+// Altrimenti se è over65 verrà applicato uno sconto del 40%
+
+// Output del prezzo finale (massimo 2 decimali)
